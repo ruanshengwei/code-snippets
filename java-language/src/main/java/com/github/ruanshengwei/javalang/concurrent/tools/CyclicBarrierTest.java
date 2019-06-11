a463b932334880f5f2a61231306191cae3de65af
ruanshengwei <200976381@qq.com>
2019-06-11 16:08:18 +0800

    1.线程池关闭demo测试

--- java-language/src/main/java/com/github/ruanshengwei/javalang/concurrent/tools/CyclicBarrierTest.java
+++ java-language/src/main/java/com/github/ruanshengwei/javalang/concurrent/tools/CyclicBarrierTest.java
@@ -2,7 +2,8 @@ package com.github.ruanshengwei.javalang.concurrent.tools;
 
 import java.util.concurrent.CyclicBarrier;
 
-/**CyclicBarrier和CountDownLatch的区别
+/**
+ * CyclicBarrier和CountDownLatch的区别
  * CountDownLatch的计数器只能使用一次，而CyclicBarrier的计数器可以使用reset()方法重
  * 置。所以CyclicBarrier能处理更为复杂的业务场景。例如，如果计算发生错误，可以重置计数
  * 器，并让线程重新执行一次。
