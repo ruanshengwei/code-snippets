a58b771f879a954f0719c966f41ee9b0a769415f
ruanshengwei <200976381@qq.com>
2019-06-17 12:00:45 +0800

    1.idea 单步调试idea（因为要显示）会调用代理类的toString()方法

--- java-language/src/main/java/com/github/ruanshengwei/javalang/proxy/dynamicproxy/ProxyFactory.java
+++ java-language/src/main/java/com/github/ruanshengwei/javalang/proxy/dynamicproxy/ProxyFactory.java
@@ -14,6 +14,10 @@ public class ProxyFactory {
     public Object getProxyInstance() {
         return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(),
                 (proxyInstance,method,args)->{
+                    //单步调试时IDEA会调用被代理类的toString()方法（展示变量的值）
+                    //例如在窗口中粘贴:method.getName() 每执行一次 控制台都会重复输出信息
+                    // 代理UserDao类会代理该类的所有方法（包括toString），因此会重复输出
+                    System.out.println("----------"+method.getName()+"-----------");
                     System.out.println("开启事务");
 
                     // 执行目标对象方法
