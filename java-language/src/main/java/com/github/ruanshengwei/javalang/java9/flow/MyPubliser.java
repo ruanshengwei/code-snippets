912d5376b331f610708e55184ee7a7787332d0c5
ruanshengwei <200976381@qq.com>
2019-06-20 18:15:44 +0800

    1.java9 flow API学习

---
+++ java-language/src/main/java/com/github/ruanshengwei/javalang/java9/flow/MyPubliser.java
@@ -0,0 +1,10 @@
+package com.github.ruanshengwei.javalang.java9.flow;
+
+import java.util.concurrent.Flow;
+
+public class MyPubliser implements Flow.Publisher<Integer> {
+    @Override
+    public void subscribe(Flow.Subscriber<? super Integer> subscriber) {
+
+    }
+}
